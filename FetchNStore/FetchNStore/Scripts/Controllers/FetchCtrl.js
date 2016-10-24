31eed79ee5eabffc7f6ceb548dc41a2a575d3210
David Cramb <david.cramb@gmail.com>
2016-10-24 17:18:47 -0500

    response for GET working and displaying result in DOM. No logic existing for dropdown yet

--- FetchNStore/FetchNStore/Scripts/Controllers/FetchCtrl.js
+++ FetchNStore/FetchNStore/Scripts/Controllers/FetchCtrl.js
@@ -1,8 +1,22 @@
 ﻿var hi = "hi";
 console.log(hi);
 app.controller('FetchCtrl', function ($scope, $http) {
+    $scope.userURL = "";
+    $scope.status = "";
+    $scope.method = "";
+    $scope.responseTime = "";
     $scope.placeholder = "http://www.reddit.com";
+
     $scope.fetchURL = () => {
         console.log("test");
+        console.log($scope.userURL);
+        $http.get($scope.userURL)
+        .then(function (response) {
+            console.log(response)
+            $scope.status = response.status;
+            $scope.method = response.config.method; 
+        });
     };
+
+
 });
